17855608b02494b637eb212e833ba0afd7a3d5c1
Sasha Waszaj <swaszajf@gmail.com>
2025-01-24 13:48:25 -0300

    info added

--- UniTask/Backend/server.js
+++ UniTask/Backend/server.js
@@ -5,12 +5,16 @@ const courseRoutes = require("./routes/course.routes");
 const examRoutes = require("./routes/exam.routes");
 const subjectRoutes = require('./routes/subject.routes');
 const GradeRoutes = require('./routes/grade.routes');
-
+require('dotenv').config();
+const helmet = require('helmet');
 require("./config/config.mongoose");
+
+const MONGO_URI = process.env.MONGO_URI;
 
 const app = express();
 const port = 8080;
 
+app.use(helmet());
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({extended:true}));
